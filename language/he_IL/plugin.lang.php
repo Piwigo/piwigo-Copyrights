c878ce7db94618a7d964992d3ddc526baf0d5bc0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:54:46 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Copyright'] = 'זכויות יוצרים';
+$lang['Copyrights'] = 'זכויות יוצרים';
+$lang['Create copyright'] = 'צור זכויות יוצרים';
+$lang['Delete copyright'] = 'מחק זכויות יוצרים';
+$lang['Edit copyright'] = 'ערוך זכויות יוצרים';
+$lang['Set copyright'] = 'הגדר זכויות יוצרים';
+$lang['This copyright already exists.'] = 'זכויות יוצרים אלו כבר קיימות.';
+$lang['Update copyright'] = 'עדכן זכויות יוצרים';
+$lang['Visible'] = 'גלוי';
+$lang['With copyright'] = 'עם זכויות יוצרים';
+$lang['Without copyright'] = 'בלי זכויות יוצרים';
