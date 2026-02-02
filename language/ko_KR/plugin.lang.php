a28b169a26cd1af8f04a80b8be5719ccabe7a323
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-02 11:45:00 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Visible'] = '표시';
+$lang['Copyright'] = '저작권';
+$lang['Copyrights'] = '저작권';
+$lang['Create copyright'] = '저작권 생성';
+$lang['Delete copyright'] = '저작권 삭제';
+$lang['Edit copyright'] = '저작권 수정';
+$lang['Set copyright'] = '저작권 설정';
+$lang['This copyright already exists.'] = '이 저작권은 이미 존재합니다.';
+$lang['Update copyright'] = '저작권 업데이트';
+$lang['With copyright'] = '저작권 있음';
+$lang['Without copyright'] = '저작권 없음';
